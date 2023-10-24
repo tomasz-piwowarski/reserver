6b46cc7c060fd4da9f99393dc87db5aaadfea170
tomasz-piwowarski <piwtom99@gmail.com>
2023-10-24 20:17:51 +0200

    Remove trycatch from server action, colliding with next.js redirect

--- frontend/reserver/src/app/actions.ts
+++ frontend/reserver/src/app/actions.ts
@@ -4,24 +4,20 @@ import { redirect } from "next/navigation";
 import { DJANGO_URL } from "../utils/consts";
 
 export const signUpUser = async (prevState: any, credentials: FormData) => {
-  try {
-    const options = {
-      method: "POST",
-      body: JSON.stringify({
-        username: credentials.get("username"),
-        password: credentials.get("password"),
-      }),
-      headers: { "Content-Type": "application/json" },
-    };
+  const options = {
+    method: "POST",
+    body: JSON.stringify({
+      username: credentials.get("username"),
+      password: credentials.get("password"),
+    }),
+    headers: { "Content-Type": "application/json" },
+  };
 
-    const response = await fetch(`${DJANGO_URL}/user/register/`, options);
+  const response = await fetch(`${DJANGO_URL}/user/register/`, options);
 
-    const data = await response.json();
+  const data = await response.json();
 
-    if (!response.ok) return { ...data, error: true };
+  if (!response.ok) return { ...data, error: true };
 
-    redirect("/api/auth/signin");
-  } catch (error: any) {
-    return error.message;
-  }
+  redirect("/api/auth/signin");
 };
